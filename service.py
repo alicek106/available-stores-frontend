e390427f106d469bf23b7016b235ae43da2244cb
alicek106 <alice_k106@naver.com>
2019-05-17 14:42:59 +0900

    Add service check page (by runtime environment)

--- service.py
+++ service.py
@@ -1,6 +1,7 @@
 from flask import Flask, session, url_for, redirect, render_template, request, abort, flash, jsonify, Response
 from flask_restful import Api
 from search_store_resource import SearchStoreResource
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,7 +9,7 @@ api.add_resource(SearchStoreResource, '/search')
 
 @app.route("/", methods=['GET'])
 def root():
-    return render_template("index.html")
+    return render_template("index.html", runtime=os.environ['RUNTIME'])
 
 # @app.route('/', defaults={'path': ''})
 # @app.route('/<path:path>')
