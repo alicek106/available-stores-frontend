dd38d50672011cbfa85cebe52d310b8e5cd277d3
alicek106 <alice_k106@naver.com>
2019-05-18 03:01:12 +0900

    Disabled jinja cache flush

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from controller import app
 from flask_bootstrap import Bootstrap
 
 
-def before_request():
-    app.jinja_env.cache = {}
+# def before_request():
+#     app.jinja_env.cache = {}
 
 
 if __name__ == '__main__':
     Bootstrap(app)
-    app.before_request(before_request)
+    # app.before_request(before_request)
     app.run(debug=True, host='0.0.0.0', port=80)
