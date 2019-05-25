232ea0bf37e2095a3119f5af5a91c3d6800277e2
alicek106 <alice_k106@naver.com>
2019-05-25 16:50:25 +0900

    Add cookie feature to sustain search location

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,48 @@
+$(document).ready(function () {
+    var location = getCookie("location");
+
+    if (location != "") {
+        $('#location').val(location).attr('selected','true')
+        console.log(location + ' is set to automatically')
+    } else {
+        console.log('cookie is empty')
+    }
+
+    $("#location").change(function () {
+            deleteCookie('location')
+            setCookie('location', $(this).val(), 7)
+            console.log('cookie is set to ' + $(this).val())
+        }
+    );
+});
+
+function setCookie(cookieName, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var cookieValue = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toGMTString());
+    document.cookie = cookieName + "=" + cookieValue;
+}
+
+function deleteCookie(cookieName) {
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() - 1);
+    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+}
+
+function getCookie(cookieName) {
+    cookieName = cookieName + '=';
+    var cookieData = document.cookie;
+    var start = cookieData.indexOf(cookieName);
+    var cookieValue = '';
+    if (start != -1) {
+        start += cookieName.length;
+        var end = cookieData.indexOf(';', start);
+        if (end == -1) end = cookieData.length;
+        cookieValue = cookieData.substring(start, end);
+    }
+    return unescape(cookieValue);
+}
+
 function query_stores() {
     var obj = new Object();
     obj.name = $('#store-name').val()
@@ -24,14 +69,14 @@ function query_stores() {
 function render_table(data, name) {
     $("#stores-table-tbody").empty();
 
-    if(data.length == 0){
+    if (data.length == 0) {
         $('#no-result-span').text('검색된 매장이 없습니다 : ' + name)
         return;
     }
 
     $('#no-result-span').text('')
 
-    for(var i = 0; i < data.length; i++){
+    for (var i = 0; i < data.length; i++) {
         append_row = '<tr><th>' + data[i][0] + '</th><td>' + data[i][2] + '</td></tr>'
         $('#stores-table-tbody').append(append_row);
     }
